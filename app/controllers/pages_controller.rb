accc3148787c83747d065548f6b0733d3abe0812
Keith Navarro <keithnavarro3@gmail.com>
2017-05-23 05:05:50 +0000

    Added usernames and fixed the navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,11 @@ class PagesController < ApplicationController
   end
 
   def profile
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice=> 'User not found!'
+    end
   end
 
   def explore
