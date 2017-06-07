a883a6fc672becad8d14de8f87aba065a93a6b5f
Keith Navarro <keithnavarro3@gmail.com>
2017-06-07 07:18:50 +0000

    FINAL

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,18 @@ class PagesController < ApplicationController
   
   # back-end code for pages/index
   def index
+    #no specific ruby code is needed for this page
   end
 
   # back-end code for pages/home
   def home
     following = Array.new
+    #checks the database for who the user is following.
     for @f in current_user.following do
       following.push(@f.id)
     end
+    
+    #only posts from users in the following array are collected.
     @posts = Post.all.where("user_id = ?", following)
     @newPost = Post.new
   end
@@ -25,12 +29,14 @@ class PagesController < ApplicationController
       redirect_to root_path, :notice=> "User not found!" 
     end
     
+    #only posts by that specific user are collected from the database
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new
   end
 
   # back-end code for pages/explore
   def explore
+    #all posts are collected from the database
     @posts = Post.all
     @newPost = Post.new
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,13 @@ class User < ActiveRecord::Base
          has_many :posts, dependent: :destroy
          has_many :active_relationships, class_name: "Relationship", foreign_key: 'follower_id', dependent: :destroy
          has_many :passive_relationships, class_name: "Relationship", foreign_key: 'followed_id', dependent: :destroy
-         
+         #active relationships is where the user is following
+         #passive is where the user is being followed
          
          has_many :following, through: :active_relationships, source: :followed
          has_many :followers, through: :passive_relationships, source: :follower
 
-         
+         #denotes the follower/following relationships
          def follow(other)
            active_relationships.create(followed_id: other.id)
          end
